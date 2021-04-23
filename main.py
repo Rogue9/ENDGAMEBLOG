61b0f98b3eed1ac7d05b09bc7c982d01e9c9222b
John Lewis <jrlewis84@gmail.com>
2021-04-22 23:18:25 -0400

    Update main.py
    ENV VARIABLES

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ from sqlalchemy import ForeignKey
 import smtplib
 import os
 EMAIL = "bulby37@gmail.com"
-PASSWORD = "U7)gEm3$.P@"
+PASSWORD = os.environ.get("EMAIL_PASS")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
